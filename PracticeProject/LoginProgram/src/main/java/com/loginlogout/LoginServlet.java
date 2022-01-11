adb8b1f7b05b75b6bd9963799e83ca1a37301238
AshaThampi269 <95743658+AshaThampi269@users.noreply.github.com>
2022-01-11 21:51:56 +0530

    Add files via upload

---
+++ PracticeProject/LoginProgram/src/main/java/com/loginlogout/LoginServlet.java
@@ -0,0 +1,39 @@
+package com.loginlogout;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		if(username.equals("Asha") && password.equals("pass"))
+		{
+			response.sendRedirect("logout.html");
+		}
+		else
+		{
+			request.getRequestDispatcher("index.html").forward(request, response);
+			response.getWriter().println("Invalid User !!");
+		}
+	}
+
+}
